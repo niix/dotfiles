078b55bc319c103ff48f70e1181b836afaf2aa1a
niix <njustice@gmail.com>
2014-01-07 09:15:37 -0800

    Move git hist to gitconfig file

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,4 +17,3 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias hist='git log --pretty=oneline --graph --date=short --decorate --abbrev-commit'
